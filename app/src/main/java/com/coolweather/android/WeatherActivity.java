c1c5d78622553fe354f79d3c3d0228c61f40ec61
zengzhuoyu <zengzhuoyu24@163.com>
2017-11-09 10:46:01 +0800

    修改程序图标和名称

--- app/src/main/java/com/coolweather/android/WeatherActivity.java
+++ app/src/main/java/com/coolweather/android/WeatherActivity.java
@@ -22,7 +22,6 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.coolweather.android.gson.Forecast;
 import com.coolweather.android.gson.Weather;
-//import com.coolweather.android.service.AutoUpdateService;
 import com.coolweather.android.service.AutoUpdateService;
 import com.coolweather.android.util.HttpUtil;
 import com.coolweather.android.util.Utility;
@@ -245,7 +244,7 @@ public class WeatherActivity extends AppCompatActivity {
         carWashText.setText(carWash);
         sportText.setText(sport);
         weatherLayout.setVisibility(View.VISIBLE);
-        
+
         Intent intent = new Intent(this, AutoUpdateService.class);
         startService(intent);
     }
